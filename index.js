8ceedb63ac4fa769b1406a0b01124ea3d170b41f
Anish Kumar <anishkumar@Anishs-MacBook-Air.local>
2023-10-08 01:03:38 +0530

    besic db conneciton made

--- index.js
+++ index.js
@@ -1,30 +1,41 @@
-require('dotenv').config()
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
-
-
+const express = require("express");
+const app = express();
+const { Sequelize } = require("sequelize");
 
 // Port no. from .env file
 const port = process.env.PORT;
 
-let login = (req, res, next)=>{
-    console.log("Login First");
-    if(req.url=='/'){
-      res.send("login First in hello world");
-      next();
-    }
+// Create an instance of sequelize class and use object of it.
+
+const sequelize = new Sequelize({
+  database: "lodgiedge",
+  username: "root",
+  password: "root@123",
+  host: "localhost",
+  dialect: "mysql",
+});
+
+// Use the .authenticate() function to test if the connection is OK:
+async function  checkConnection(){
+  try {
+    await sequelize.authenticate();
+    console.log("Connection has been established successfully.");
+  } catch (error) {
+    console.error("Unable to connect to the database:", error);
+  }
 }
 
-let authorise = (req, res, next)=>{
-  console.log("are your authorised");
-  next();
-}
+checkConnection();
 
-app.get('/',login,authorise,(req, res) => {
-  res.send('Hello World!')
-})
+
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
