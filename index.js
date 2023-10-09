c9d28b6293932484f60938c72db99f12791a522c
Anish Kumar <anishkumar@Anishs-MacBook-Air.local>
2023-10-09 19:09:13 +0530

    sequelize-cli commands and migration done. All about migration and table creation.

--- index.js
+++ index.js
@@ -1,83 +1,20 @@
-require("dotenv").config();
-
-const express = require("express");
+const express = require ('express');
 const app = express();
-const { Sequelize, DataTypes } = require("sequelize");
+const port = 8080;
 
-// Port no. from .env file
-const port = process.env.PORT;
+const {Customer} = require("./models/index");
 
-// Create an instance of sequelize class and use object of it.
+app.get('/',(req, res)=> {
+    res.send("<h1>Express server sending data to frontend</h1>")
 
-const sequelizeObj = new Sequelize({
-  database: "lodgiedge",
-  username: "root",
-  password: "root@123",
-  host: "localhost",
-  dialect: "mysql",
-});
-
-// Use the .authenticate() function to test if the connection is OK:
-async function  checkConnection(){
-  try {
-    await sequelizeObj.authenticate();
-    console.log("Connection has been established successfully.");
-  } catch (error) {
-    console.error("Unable to connect to the database:", error);
-  }
-}
-checkConnection();
-
- // Define the model by using .define method with sequelize object. It will create a table in database.
-const userObject = sequelizeObj.define('User',{
-  id: {
-    type: DataTypes.INTEGER,
-    autoIncrement: true,
-    primaryKey: true
-  },
-  name : {
-    type: DataTypes.STRING,
-    allowNull: false
-  },
-  email: {
-    type: DataTypes.STRING,
-    allowNull: false,
-
-  }
 })
 
-// Create table, insert and show  data in the table created.
-async function addData(){
-  try{
+app.listen(port,async ()=>{
+    console.log(`Express server is listening to port ${port}`);
 
-    // sync is necessary for creation of table. If it is true. It will drop table and create a new one
-    // await sequelizeObj.sync();
+    let Customer1 = await  Customer.create({custName:"Anish Kumar", custAddr:"Delhi"});
+    // console.log(Customer1);
 
-    // insert into tables created
-    // await userObject.create({name: "Eklavya", email: "akjnv541@gmail.com"})
-    await userObject.create({name: "Ekta", email: "akjnv542@gmail.com"})
-    
-    // Show data
-    let data = await userObject.findAll();
-    console.log(data);
-
-    
-  }
-  catch{
-    console.log("something went wrong")
-  }
-}
-
-addData();
-
-
-
-
-
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
-
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
-});
+    let data = await Customer.findAll();
+    // console.log(data);
+})
---
+++ migrations/20231009123122-create-customer.js
@@ -0,0 +1,40 @@
+'use strict';
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Customers', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      custName: {
+        type: Sequelize.STRING
+      },
+      custAddr: {
+        type: Sequelize.STRING
+      },
+      custPan: {
+        type: Sequelize.STRING
+      },
+      custGst: {
+        type: Sequelize.STRING
+      },
+      custStatus: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Customers');
+  }
+};
